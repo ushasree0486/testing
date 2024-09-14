e035e1c2a8b8517c72aaba293869349c58c95766
Usha J <ushasree.jpl@gmail,.com>
2024-09-14 07:46:51 +0530

    cr-123 this is test commit

--- src/main/java/org/usha/java/Main.java
+++ src/main/java/org/usha/java/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("This is my first git hub commit integrating with jenkins");
+        System.out.println("this is commit from feature in to master" );
     }
 }
