1f5a57e244488f9412a4d7205d8fec967813e74e
ushasree0486 <ushasree.jpl@gmail.com>
2024-09-16 09:05:47 +0530

    Update Main.java
    demo for rebase

--- src/main/java/org/usha/java/Main.java
+++ src/main/java/org/usha/java/Main.java
@@ -3,5 +3,7 @@ package org.usha.java;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("This demo is for git rebase from intellij");
+ 
     }
 }
