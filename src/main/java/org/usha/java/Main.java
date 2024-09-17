d61bf0de0e930d5e3f40c324d9cf729fd167db64
Usha J <ushasree.jpl@gmail,.com>
2024-09-17 14:29:53 +0530

    cr-123 this is test commit for rebase

--- src/main/java/org/usha/java/Main.java
+++ src/main/java/org/usha/java/Main.java
@@ -1,7 +1,7 @@
 package org.usha.java;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args)s{
         System.out.println("Hello world!");
         System.out.println("This is my first git hub commit integrating with jenkins");
         System.out.println("this is commit from feature in to master" );
